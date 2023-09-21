c6ba5c3b1758a5e9715889126e93245cf1c6f7fb
Vít Ružička <r201@email.cz>
2023-09-21 11:26:46 +0200

    a

---
+++ prvnimore/src/prvnimore/Prvnimore.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prvnimore;
+
+/**
+ *
+ * @author ruzickaVI
+ */
+public class Prvnimore {
+
+    /**
+     * @param args the command line arguments
+     */
+
+     public static void prohod(int jedna, int dva){
+        jedna = jedna + dva;
+        dva = jedna - dva;
+        jedna = jedna - dva;
+        
+        System.out.println("jedna je: " + jedna   + " " + "druha je: " + dva);
+     }
+    public static void main(String[] args) {
+        int jedna = 5;
+        int dva = 1;
+        
+        System.out.println("Původní hodnota proměnné jedna je: " + jedna + " a původní hodnota proměnné dva je: " + dva);
+        prohod(jedna, dva);
+    }
+    
+}
