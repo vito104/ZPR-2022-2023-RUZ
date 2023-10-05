fa30b5b0f1a5d3619a644c5148168b4963677094
Vít Ružička <r201@email.cz>
2023-10-05 10:29:36 +0200

    a

---
+++ 5.10.2023/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,17 @@
+
+package javaapplication2;
+
+/**
+ *
+ * @author ruzickavi
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
