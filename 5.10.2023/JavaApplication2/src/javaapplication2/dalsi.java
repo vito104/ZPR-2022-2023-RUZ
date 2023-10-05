0517a3a607822e91b255eeb02fd5a6d55ebd1372
Vít Ružička <r201@email.cz>
2023-10-05 12:25:05 +0200

    fdfdfdafa

--- 5.10.2023/JavaApplication2/src/javaapplication2/dalsi.java
+++ 5.10.2023/JavaApplication2/src/javaapplication2/dalsi.java
@@ -5,8 +5,32 @@ import java.util.Scanner;
 
 public class dalsi {
 
-    Scanner scanner = new Scanner(System.in);
-    int body = scanner.nextInt();
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadejte body: \n");
+        int body = scanner.nextInt();
+        int znamka = 0;
+        if(body > 100){
+            System.out.println("Maximum je 100 bodů");
+        } else{
+            
+            if(body > 91){
+                znamka = 1;
+            }
+            else if(body < 91){
+                znamka = 2;
+            }
+            else if (body < 81){
+                znamka = 3;
+            }
+            else if(body < 61){
+                znamka = 4;
+            }
+            else if(body < 51){
+                znamka = 5;
+            }
+            System.out.println("Výsledná známka: " + znamka);
+        }
+    }
 
 }
