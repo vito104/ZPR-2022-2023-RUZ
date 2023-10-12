e52aae2a5bfbd6a46ff1380e673469181ff40fea
Vít Ružička <r201@email.cz>
2023-10-12 11:03:25 +0200

    fdfadsfsfs

---
+++ 12.10/prvni/src/prvni/App.java
@@ -0,0 +1,18 @@
+package prvni;
+
+public class App {
+        public static void main(String[] args) {
+            int[] a = {1,2,3,4};
+            if(a[0] < a[1]){
+                System.out.println(a[1] + "je největší");
+            } else if(a[0]< a[2]){
+                System.out.println(a[2] + "je největší");
+            }else if(a[0] < a[3]){
+                System.out.println(a[3] + "je největší");
+            }else{
+                System.out.println(a[0] + "je největší");
+            }
+            
+        }
+    }
+    
--- 4prom/JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ 4prom/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -1,3 +1,4 @@
+package javaapplication3;
 
 public class JavaApplication3 {
     public static void main(String[] args) {
