ab9f3a2ec50b7e4c64e5afc61d47ab7b1239a1d3
Vít Ružička <r201@email.cz>
2023-10-19 10:02:14 +0200

    fdafasf

--- ukol/ukol/src/App.java
+++ ukol/ukol/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int podil = 0;
+        int zbytek = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadejte prvni cislo: ");
+        int iCis1= sc.nextInt();
+
+        System.out.println("Zadejte druhe cislo: ");
+        int iCis2 = sc.nextInt();
+
+        zbytek = iCis1;
+        while(zbytek>= iCis2){
+            zbytek = zbytek - iCis2;
+            podil++;
+        }
     }
 }
