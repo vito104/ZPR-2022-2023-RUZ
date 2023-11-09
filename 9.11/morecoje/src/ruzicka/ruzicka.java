b4e9da169efa1a78bbd86929c0ae4dbf466ebcee
Vít Ružička <r201@email.cz>
2023-11-09 11:27:05 +0100

    fdfasfdsa

--- 9.11/morecoje/src/ruzicka/ruzicka.java
+++ 9.11/morecoje/src/ruzicka/ruzicka.java
@@ -1,18 +1,27 @@
 package ruzicka;
 
 import  java.util.Scanner;
+import java.util.Arrays;
+
 
 public class ruzicka {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int a2 = 0;
+       /*  int a2 = 0;
         int zb = 0;
         while (a > 0){
             zb = a % 10;
             a = a / 10;
             a2 = (a2 * 10) + zb; 
         }
-        System.out.println(a2);
+        System.out.println(a2);*/
+        char[] cisla = String.valueOf(a).toCharArray();
+        int max = cisla.length;
+        System.out.println(max);
+        int i;
+        for(i = (max -1); i >= 0; i--){
+            System.out.println(cisla[i]);
+        }
     }
 }
